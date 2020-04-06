305b379327767e8d2731cc6e007c276e75ad46e2
Asifuzzaman Opi <51073544+0ne1ronaut@users.noreply.github.com>
2020-04-06 23:50:49 +0600

    Update Basic Linked List Implementation.cpp

--- Linked List/Basic Linked List Implementation.cpp
+++ Linked List/Basic Linked List Implementation.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 struct node
 {
@@ -50,7 +51,7 @@ void print()
 void delete_node (int data)
 {
     node *current_node = root;
-    node previous_node = NULL;
+    node *previous_node = NULL;
     //searching for the node
     while (current_node->data != data)
     {
@@ -75,16 +76,34 @@ void delete_node (int data)
     }
 }
 
+void add_node (int data1, int data2, int data)
+{
+    node *current_node = root;
+    node *previous_node = NULL;
+    //searching for these two nodes
+    while (current_node->data != data1 && current_node->next->data != data2)
+    {
+        previous_node = current_node;
+        current_node = current_node->next;
+    }
+    //creating a new node
+    node *newnode = new node();
+    newnode->data = data;
+    //injecting new node between two nodes
+    newnode->next = current_node->next;
+    current_node->next = newnode;
+}
+
 int main()
 {
     append(1);
     append(2);
+    add_node(2, 3, 5);
     append(3);
     print();
     return 0;
 }
 
-Thanks to : https://www.shafaetsplanet.com/planetcoding/?p=2689
 
 
 
