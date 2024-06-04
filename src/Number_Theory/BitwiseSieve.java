92edd502d950e929f313cab7fc1765850347f69f
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2024-06-04 17:01:01 +0600

    implementation of sieve of eratosthenes (bitwise operation)

--- src/Number_Theory/BitwiseSieve.java
+++ src/Number_Theory/BitwiseSieve.java
@@ -2,7 +2,14 @@ package Number_Theory;
 
 public class BitwiseSieve {
   public static void main(String[] args) {
+    int N = 100;
 
+    //if the number was 96 then 96/32 = 3 then 3 would be enough
+    //since N is equal to 100 or more that's why it's safe to add another index hence the + 1
+    //right shifting N by 5 and dividing N / 32 are same thing
+    int[] status = new int[(N >> 5) + 1];
+
+    sieve(N, status);
   }
 
 
@@ -14,4 +21,25 @@ public class BitwiseSieve {
     return N | (1 << pos);
   }
 
+  public static void sieve(int N, int[] status) {
+    int i, j, sqrtN;
+
+    sqrtN = (int) Math.sqrt(N);
+
+    for (i = 3; i <= sqrtN; i += 2) {
+      if (!check(status[i >> 5], i % 32)) {
+        for (j = i * i; j <= N; j += 2 * i) {
+          status[j >> 5] = set(status[j >> 5], j & 31);
+        }
+      }
+    }
+
+    System.out.println(2);
+
+    for (i = 3; i <= N; i += 2) {
+      if (!check(status[i >> 5], i & 31)) {
+        System.out.println(i);
+      }
+    }
+  }
 }
