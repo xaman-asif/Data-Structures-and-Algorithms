13b9bd10b15c2b4649ead3f1ccf91e66e55e36ba
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2024-06-06 19:03:23 +0600

    implementation of sieve of eratosthenes (partial)

--- src/Number_Theory/Sieve_of_Eratosthenes4.java
+++ src/Number_Theory/Sieve_of_Eratosthenes4.java
@@ -1,19 +1,45 @@
 package Number_Theory;
 
 public class Sieve_of_Eratosthenes4 {
-  public static int[] primeGenerator(int n) {
-    int root = (int) Math.sqrt(n);
+  public static int[] primeGenerator(int N) {
+    int root = (int) Math.sqrt(N);
+    int[] bits = new int[(N >> 5) + 1];
 
+//    bits[1 >> 5] = setBit(1 >> 5, (1 >> 5) & 31);
+//    bits[2 >> 5] = setBit(2 >> 5, (2 >> 5) & 31);
 
+    for (int i = 4; i <= N; i += 2) {
+      bits[i >> 5] = setBit(bits[N >> 5], i & 31);
+    }
 
-    return null;
+    for (int i = 3; i <= root; i += 2) {
+      if (!checkBit(bits[i >> 5], i & 31)) {
+        for (int j = i * i; j <= N; j += i) {
+          bits[j >> 5] = setBit(bits[j >> 5], j & 31);
+        }
+      }
+    }
+
+    return bits;
   }
 
-  public int setBit(int num, int pos) {
+  public static void main(String[] args) {
+    int[] primes = primeGenerator(50);
+
+    for (int i = 3; i <= (int) Math.sqrt(50); i += 2) {
+      if (checkBit(primes[i >> 5], i & 31)) {
+        System.out.println(i);
+      }
+    }
+  }
+
+
+
+  public static int setBit(int num, int pos) {
     return num | (1 << pos);
   }
 
-  public boolean checkBit(int num, int pos) {
-    return (num & (1 << pos)) > 0;
+  public static boolean checkBit(int num, int pos) {
+    return (num & (1 << pos)) == 0;
   }
 }
